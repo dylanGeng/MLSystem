fa022cd6034f35a1fac6e5d37767d29acc1f0413
dylan <dylangeng@163.com>
2015-08-27 09:31:02 +0800

    make a new resop

---
+++ Chapter2/Chapter2.py
@@ -0,0 +1,75 @@
+﻿from matplotlib import pyplot as plt
+from sklearn.datasets import load_iris
+import numpy as np
+
+
+# We load the data with load_iris from sklearn
+data = load_iris()
+features = data['data']
+features_bk = data['data']
+feature_names = data['feature_names']
+target = data['target']
+target_name = data['target_names']
+labels = np.array([target_name[i] for i in target])
+
+for t,marker,c in zip(range(3),">ox","rgb"):
+    #We plot each class on its own to get different colored markers
+    plt.scatter(features[target == t, 0],
+                features[target == t, 2],
+                marker=marker,
+                c=c)
+plt.show()
+
+
+plength = features[:,2]
+#use numpy operation to get setosa features
+is_setosa = (target == 0)
+#This is the important step:
+max_setosa = plength[is_setosa].max()
+min_non_setosa = plength[~is_setosa].min()
+print('Maximum of setosa: {0}.'.format(max_setosa))
+print('Minimum of others: {0}.'.format(min_non_setosa))
+
+features = features[~is_setosa]
+labels = labels[~is_setosa]
+virginica = (labels == 'virginica')
+
+best_acc = -1.0
+best_fi = 0.0
+best_t = 0.0
+for fi in range(features.shape[1]):
+    #We are going to generate all possible threshold for this feature
+    thresh = features[:,fi].copy()
+    thresh.sort()
+    # Now test all thresholds:
+    for t in thresh:
+        pred = (features[:,fi] > t)
+        acc = (pred == virginica).mean()
+        if acc > best_acc:
+            best_acc = acc
+            best_fi = fi
+            best_t = t
+
+print(best_acc)
+print(best_fi)
+print(best_t)
+
+for t,marker,c in zip(range(1,3),"ox","gb"):
+    #We plot each class on its own to get different colored markers
+    plt.scatter(features_bk[target == t,3],
+                features_bk[target == t,2],
+                marker=marker,
+                c=c)
+plt.xlabel(feature_names[3])
+plt.ylabel(feature_names[2])
+plt.show()
+
+error = 0.0
+for ei in range(len(features)):
+    #select all but the one at position 'ei'
+    training = np.ones(len(features),bool)
+    training[ei] = False
+    testing = ~training
+    model = learn_model(features[training],virginica[training])
+    predictions = apply_model(features[testing], virginica[testing], model)
+    error += np.sum(predictions != virginica[testing])
---
+++ Chapter3/Chapter3.py
@@ -0,0 +1,71 @@
+﻿from sklearn.feature_extraction.text import CountVectorizer
+import os
+import math
+
+import scipy as sp
+import sys
+import nltk.stem
+
+english_stemmer = nltk.stem.SnowballStemmer('english')
+
+class StemmedCountVectorizer(CountVectorizer):
+    def build_analyzer(self):
+        analyzer = super(StemmedCountVectorizer,self).build_analyzer()
+        return lambda doc: (english_stemmer.stem(w) for w in analyzer(doc))
+
+
+
+def tfidf(term, doc, docset):
+    tf = float(doc.count(term))/sum(doc.count(w) for w in set(doc))
+    idf = math.log(float(len(docset))/(len([doc for doc in docset if term in doc])))
+    return tf * idf
+
+a , abb, abc = ["a"], ["a", "b", "b"], ["a", "b", "c"]
+D = [a, abb, abc]
+print(tfidf("a", a, D))
+print(tfidf("b", abb, D))
+print(tfidf("a", abc, D))
+print(tfidf("b", abc, D))
+print(tfidf("c", abc, D))
+
+def dist_raw(v1, v2):
+    delta = v1 - v2
+    return sp.linalg.norm(delta.toarray())
+
+def dist_norm(v1, v2):
+    v1_normalized = v1/sp.linalg.norm(v1.toarray())
+    v2_normalized = v2/sp.linalg.norm(v2.toarray())
+    delta = v1_normalized - v2_normalized
+    return sp.linalg.norm(delta.toarray())
+
+DIR = "post"
+posts = [open(os.path.join(DIR, f)).read() for f in os.listdir(DIR)]
+#vectorizer = CountVectorizer(min_df=1, stop_words='english')
+vectorizer = StemmedCountVectorizer(min_df=1, stop_words='english')
+print(sorted(vectorizer.get_stop_words())[0:20])
+X_train = vectorizer.fit_transform(posts)
+num_samples, num_features = X_train.shape
+print("#sample: %d, #features: %d" %(num_samples, num_features))
+print(vectorizer.get_feature_names())
+
+new_post = "imaging databases"
+new_post_vec = vectorizer.transform([new_post])
+print(new_post_vec.toarray())
+
+best_doc = None
+best_dist = sys.maxsize
+best_i = None
+for i in range(0, num_samples):
+    post = posts[i]
+
+    if post == new_post:
+        continue
+    post_vec = X_train.getrow(i)
+    d = dist_norm(post_vec, new_post_vec)
+    print("=== Post %i with dist=%.2f: %s"%(i, d, post))
+    if d < best_dist:
+        best_dist = d
+        best_i = i
+
+print("Best post is %i with dist=%.2f"%(best_i, best_dist))
+
